1d07337ecc9f16326963a88250d468195cc5dd8e
Jefferson Calmon <jeffjeff0197@gmail.com>
2023-07-08 02:29:55 -0300

    :bug: fix(git): untrack files contained in the .gitignore file

--- dist/constants/defaults.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.defaultOptions = void 0;
-exports.defaultOptions = {
-    httpProxyOptions: {
-        xfwd: true,
-        secure: process.env.NODE_TLS_REJECT_UNAUTHORIZED !== '0',
-    },
-};
