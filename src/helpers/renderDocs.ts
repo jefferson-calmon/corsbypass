1739a720547d69262e0721899dbeabff435e3bd7
Jefferson Calmon <jeffjeff0197@gmail.com>
2023-07-08 02:40:06 -0300

    :bug: fix(renderdocs): Render docs by html string also html file

--- src/helpers/renderDocs.ts
+++ src/helpers/renderDocs.ts
@@ -2,6 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 import { Headers, Response } from '../types';
+import { docs } from '../constants/docs';
 
 export function renderDocs(headers: Headers, response: Response) {
   const docFilePath = path.resolve(__dirname, '../assets/docs.html');
@@ -10,7 +11,7 @@ export function renderDocs(headers: Headers, response: Response) {
 
   headers['content-type'] = isHtml ? 'text/html' : 'text/plain';
 
-  fs.readFile(docFilePath, 'utf8', function (err, data) {
+  fs.readFile(docs, 'utf8', function (err, data) {
     if (err) {
       console.error(err);
       response.writeHead(500, headers);
